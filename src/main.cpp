59e11ca1f65de0ac4cf25869f1ebbe9649def61a
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-10 19:58:21 -0300

    nova lib USART

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,16 @@
-#include "external_int.hpp"
 #include "util/delay.h"
-#include "IO.hpp"
-#include "liquidcrystal.hpp"
-#include "adc_comp.hpp"
+#include <avr/pgmspace.h>
+#include <usart.hpp>
 
-using namespace external_int;
-using namespace liquidcrystal;
-int tick = 0;
-int comp_std = 0;
+const char text[] = "HELLO WORLD!\n\0";
 int main(){
-	digitalIO::DigitalIO(3).output().set_low();
-	Liquidcrystal my_lcd(Lcd2EN_4bits(12,13,4,11,10,9,8));
-	external_int::External_int()
-		.event_on(_INT0_, DOWN, []()-> void{
-			tick++;
-		});
-		
-	comparator::Adc_comparator().set_event(comparator::ANY, []()-> void{
-		if(comparator::Adc_comparator().get_output()){
-			digitalIO::DigitalIO(3).set_high();
-			comp_std = 1;
-		}
-		else{
-			digitalIO::DigitalIO(3).set_low();
-			comp_std = 0;
-		}
-	})
-	.enable();
-
+	usart::Usart my_output;
+	my_output.enable();
 	for(;;){
-		my_lcd.init(40, 4)
-		.set_cursor(0,0)
-		.write("- TICKS: ")
-		.set_cursor(0, 10)
-		.write(tick)
-		.set_cursor(2, 0)
-		.write("COMP:")
-		.set_cursor(2, 10)
-		.write(comp_std);
+		for(int i = 0; text[i] != '\0'; i++){
+			my_output.transmit(text[i]);
+		}
+		_delay_ms(1000);
 	}
 	return 0;
 } 
