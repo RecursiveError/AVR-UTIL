0067a68a5dcacadf6df45b9728e7765e61eda69a
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-10 18:23:52 -0300

    lib para um display de 7 segmentos 4 digitos

---
+++ lib/disp7seg/display7seg.hpp
@@ -0,0 +1,29 @@
+#ifndef _7_SEG_DISP_HPP
+#define _7_SEG_DISP_HPP
+#include <stdint.h>
+#include "IO.hpp"
+
+
+namespace display7seg{
+    class Display {
+        private:
+            digitalIO::DigitalIO _segments[7];
+            digitalIO::DigitalIO _digits[4];
+            uint8_t _number[5]{0,0,0,0};
+            uint8_t disp{0};
+            void send(uint8_t value);
+            
+        public:
+            Display(uint8_t A, uint8_t B, uint8_t C, uint8_t D, uint8_t E, uint8_t F, uint8_t G,
+                uint8_t digit1, uint8_t digit2 = IO_NULL_PORT, uint8_t digit3 = IO_NULL_PORT, uint8_t digit4 = IO_NULL_PORT):
+            _segments{A,B,C,D,E,F,G},
+            _digits{digit1, digit2, digit3, digit4}
+            {}
+            Display& init();
+            Display& write(uint8_t value, uint8_t posi);
+            Display& write(uint16_t value);
+            Display& update(); 
+    };
+}
+
+#endif
