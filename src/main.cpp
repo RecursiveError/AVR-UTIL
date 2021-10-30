4a95a7ef5d7c3bbbfad74c60b24aeb94cd5170f5
guilherme Silva Schultz <guilhermeSschultz.contato@gmail.com>
2021-10-30 19:20:14 -0300

    novo exemplo

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,40 @@
 #include <util/delay.h>
 #include "display7seg.hpp"
+#include "liquidcrystal.hpp"
 #include "usart.hpp"
 #include "adc.hpp"
-
-void usart_print_num(unsigned int value);
+#include "interrupt.hpp"
+#include "IO.hpp"
 
 int main(){
-	uint16_t result{0};
-	adc::Adc scanner;
-	usart::Usart output;
-	display7seg::Display my_display(7, 6, 5, 4, 3, 2, 19, 15, 16, 17, 18);
-	my_display.init();
-	scanner.init(adc::AVCC);
-	output.enable();
+	digitalIO::DigitalIO red(7);
+	digitalIO::DigitalIO blue(5);
+	digitalIO::DigitalIO green(6);
+	red.output().set_high();
+	blue.output().set_high();
+	green.output().set_high();
+
+	int result_lux = 0;
+
+	adc::Adc scanner(adc::AVCC, 7, false);
+	liquidcrystal::Liquidcrystal my_lcd(liquidcrystal::Lcd_4bits(13,12,8,9,10,11));
+	my_lcd.init(16, 2)
+	  .write("LuximetroDigital")
+	  .set_cursor(1,0)
+	  .write("Lux:\0");
 	for(;;){
-		result = scanner.read(adc::ADC0);
-		my_display.write(result);
-		my_display.update();
-		usart_print_num(result);
-		_delay_ms(1);
+		result_lux = scanner.read(adc::ADC0);
+		my_lcd.set_cursor(1,10).write(result_lux);
+		if(result_lux > 550){
+			green.set_high();
+			red.set_low();
+		}else{
+			red.set_high();
+			green.set_low();
+		}
+		_delay_ms(75);
 	}
+
 	return 0;
 } 
 
@@ -29,9 +44,9 @@ void usart_print_num(unsigned int value){
     uint8_t C = (value/100)%10;
     uint8_t D = (value/10)%10;
     uint8_t U = (value/1)%10;
-	usart::Usart().transmit(M+'0');
-	usart::Usart().transmit(C+'0');
-	usart::Usart().transmit(D+'0');
-	usart::Usart().transmit(U+'0');
-	usart::Usart().transmit('\n');
+	usart::Usart().transmit(M+'0')
+	.transmit(C+'0')
+	.transmit(D+'0')
+	.transmit(U+'0')
+	.transmit('\n');
 }
